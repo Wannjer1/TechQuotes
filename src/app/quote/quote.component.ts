0bd759444250acd9c7f0466493771aed731d6ced
Ann Wanjeri <wannjeri2@gmail.com>
2022-04-11 11:31:59 +0300

    added more quotes to quote component html

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -16,6 +16,30 @@ export class QuoteComponent implements OnInit {
       sender: 'Vivian Chemu',
       votes: 0,
     },
+
+    {
+      quote:
+        'The advance of technology is based on making it fit in so that you do not really even notice it, so it is part of everyday life.',
+      author: 'Bill Gates',
+      sender: 'Albert Ndegwa',
+      votes: 0,
+    },
+
+    {
+      quote:
+        'It has become appallingly obvious that our technology has exceeded our humanity.',
+      author: 'Albert Einstein',
+      sender: 'Vivian Chemu',
+      votes: 0,
+    },
+
+    {
+      quote:
+        'It has become appallingly obvious that our technology has exceeded our humanity.',
+      author: 'Albert Einstein',
+      sender: 'Vivian Chemu',
+      votes: 0,
+    },
   ];
   constructor() {}
 
