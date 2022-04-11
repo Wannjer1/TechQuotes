afa0452f0d87e01fa297cf888c56b453a2add1e1
Ann Wanjeri <wannjeri2@gmail.com>
2022-04-12 00:09:34 +0300

    bg-bug fix form new quote entry

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,7 @@
 export class Quote {
   constructor(
     public count: number,
-    public quote: string,
+    public quoteDescription: string,
     public author: string,
     public sender: string // public votes: number
   ) {}
